690c8b01d621f5ce423c328d7e59da1b24217951
1083833942@qq.com <sunny@ubuntu.(none)>
2016-12-14 14:46:04 +0800

    transport init information

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "make_log.h"
+
+int main(int argc, char *argv[])
+{
+
+    LOG("111", "222", "nihao");
+        LOG("111", "222", "nihuai");
+    LOG("111", "222", "nihaohuai");
+	rop_set_key_lifecycle
+	return 0;
+}
